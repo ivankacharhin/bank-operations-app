6d4886917eb1c88c0455a8f5058f51d7129837d3
ivankacharhin <youngvace@gmail.com>
2025-12-20 01:23:54 +0300

    Добавлен вывод общей информации о задании

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Вариант 7: Банковские операции" << std::endl;
+    std::cout << "Автор: [Ваше Имя] [Ваша Фамилия]" << std::endl;
+
+    return 0;
+}
